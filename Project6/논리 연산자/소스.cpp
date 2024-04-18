9efd1c9b94bd1912364ad55c771eb73fad2f623c
HAE-CHANG LEE <gockdd1677@gmail.com>
2024-04-19 00:00:59 +0900

    논리연산자 끝

--- Project6/논리 연산자/소스.cpp
+++ Project6/논리 연산자/소스.cpp
@@ -14,7 +14,29 @@ int main()
 	
 	int num0 = 10, num1(10);
 	int num2 = 20, num3(30);
-	if (num0 == num1 && num2 != num3)
+
+	// 연산자 우선순위를 명시화 하기 위해 괄호를 적극 활용 하였다.
+	if ( (num0 == num1) && (num2 != num3))
 		cout << "다 true" << endl;
+
+
+	// 2) ||
+	cout << (true || true) << endl; // ture
+	cout << (true || false) << endl; // true
+	cout << (false || true) << endl;  // true
+	cout << (false || false) << endl; // false
+
+	if ((num0 == num1) || (num2 == num3))
+		cout << "OR" << endl;
+
+
+	// 3) !
+	if (!(num0 != num1))
+		cout << "!" << endl;
+
+	// result가 boolean이 아닌경우
+	cout.setf(ios_base::boolalpha);
+	cout << !10 << endl;
+	cout << !(2 && 1) << endl;
 
 }
