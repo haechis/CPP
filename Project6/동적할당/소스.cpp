80bb0d2a326e467c0f3a41d10d74a4c8e4c4cce1
HAE-CHANG LEE <gockdd1677@gmail.com>
2024-05-13 23:48:59 +0900

    동적할당 스터디

--- Project6/동적할당/소스.cpp
+++ Project6/동적할당/소스.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/*
 int main()
 {
 	int n; // 지역 변수
@@ -29,5 +30,85 @@ int main()
 	*pNum = 1;
 	delete pNum; // 항상 사용하고 나서는 해제를 해야함.
 	// 그렇지 않으면 메모리가 계속 늘어남!
+	// -> 메모리 누수.
+	int* pNum0 = new int(123);
+	int* pNum1 = pNum0;
+
+	cout << *pNum0 << endl; // 123
+	delete pNum0;
+
+	cout << *pNum0 << endl; // ?
+	//  출력이 안됨. 
+	cout << *pNum1 << endl; // 이것도 쓰레기값이 나옴.
+	// pNum1이 똑같은 주소를 참조하므로,
+
+	delete pNum0; // 또 지울수있음. 유효하지 않은 포인터를 지우는 것, -> 굉장히 주의해야 함.!!
+
+
+
+
+}
+*/
+
+/*
+int main()
+{
+	int* pNum;
+	{
+		int n = 10;
+		pNum = &n;
+	}
+	// n은 이미 {} 안에서 해제가 되었음.
+	// 블록 밖에서 포인터를 사용하면 위험할 수 있음.
+	cout << *pNum << endl;
+}
+*/
+struct Person
+{
+	float weight;
+	float height;
+};
+
+
+int main()
+{
+	// 동적 배열
+	const int s = 10;
+	int arr[s] = { 1,2,3 };
+
+	// 이런 건 안됐음.
+	/*
+	int s;
+	cin >> s;
+	int arr[s] = { 1,2,3 }
+	*/
+
+	// 동적할당을 사용하면 가능함!
+	int num;
+	cout << "숫자 입력하시오" << endl;
+	cin >> num;
+	int* arr0 = new int[num];
+	arr0[0] = 1;
+	cout << arr0[0] << endl;
+
+	delete[] arr0;
+
+	// 구조체
+	Person* person = new Person{ 74.1f, 180.2f };
+	cout << person->weight << endl;
+	cout << (*person).height << endl; // 표현 방법 두 개임.
+
+	// person 여러명 만들려면 이렇게도 가능함.
+	Person* per = new Person[10];
+	for (int i = 0; i < 10; i++)
+	{
+		per[i].height = i * 10;
+		cout << per[i].height << endl;
+		// cout << per[i]->weight << endl;
+	}
+	
+	delete[] per;
+	delete person;
+	
 
 }
