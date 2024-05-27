1e95690ac1d892739b61ce82f5efb85be4723d03
HAE-CHANG LEE <gockdd1677@gmail.com>
2024-05-27 23:08:15 +0900

    변수로 전달

--- Project6/함수_값으로 전달/소스.cpp
+++ Project6/함수_값으로 전달/소스.cpp
@@ -12,6 +12,25 @@ void swap(int px, int py)
 	py = temp;
 }
 
+struct Weapon
+{
+	int price;
+	int power;
+};
+
+void upgrade(Weapon weapon)
+{
+	weapon.power += 10;
+	weapon.price += 10;
+}
+
+Weapon Upgrade(Weapon weapon)
+{
+	weapon.power += 10;
+	weapon.price += 10;
+	return weapon;
+}
+
 int main()
 {
 	int x = 10,  y = 20;
@@ -30,4 +49,22 @@ int main()
 
 	cout << "x addr : " << &x << endl;
 	cout << "y addr : " << &y << endl;
+
+	cout << "\n\n두번째 예제, Weapon이라는 Struct를 만들어서 Call by value 확인!" << endl;
+	Weapon weapon{ 10,20 };
+	cout << "Upgrade 전" << endl;
+	cout << "price: " << weapon.price << endl;
+	cout << "power: " << weapon.power << endl<<endl;
+	upgrade(weapon);
+	cout << "Upgrade 후 - void 함수 활용" << endl;
+	cout << "price: " << weapon.price << endl;
+	cout << "power: " << weapon.power << endl;
+
+	cout << "반영하려면 return값을 받으면 됨. (Call by value에서)" << endl;
+	weapon = Upgrade(weapon);
+	cout << "Upgrade 후 - return 활용" << endl;
+	cout << "(단점: 큰 파라미터는 오버헤드 발생 가능)" << endl;
+	cout << "(장점: 리턴 없는 함수를 볼 때, 값이 바뀌지 않는 것을 알 수 있음! => 협업 시 큰 도움이 됨.)" << endl;
+	cout << "price: " << weapon.price << endl;
+	cout << "power: " << weapon.power << endl;
 }
